4d04e9244d0b6a2535cba6431f5401d1a819cdc8
jon-tj <jh.tjemsland@gmail.com>
2023-10-02 14:33:22 +0200

    edit countdown

--- Flask/static/js/countdown.js
+++ Flask/static/js/countdown.js
@@ -10,14 +10,26 @@ setInterval(() => {
         countdown.timeRemaining--;
         var minutesRemaining=Math.floor(countdown.timeRemaining/60);
         var secondsRemaining=countdown.timeRemaining-minutesRemaining*60;
-        countdown.element.innerHTML=`${minutesRemaining}:${secondsRemaining}`
+        countdown.element.value=`${minutesRemaining}:${secondsRemaining}`;
         
-        if(countdown.timeRemaining==0) fetch("/api/fire")
+        if(countdown.timeRemaining==0) fetch("/api/fire");
     }
 }, 1000);
 function toggleCountdown(sender){
     countdown.enabled=!countdown.enabled;
     if(countdown.enabled)
-        sender.innerHTML="Pause countdown"
-    else sender.innerHTML="Start countdown"
+        sender.innerHTML="Pause countdown";
+    else sender.innerHTML="Start countdown";
 }
+countdown.element.addEventListener("change",()=>{
+    var s=countdown.element.value.split(":");
+    var sec=parseInt(s[1])
+    var min=parseInt(s[0])
+    if(s.length==2 &! isNaN(sec) &! isNaN(min)){
+        countdown.timeRemaining=min*60+sec;
+        countdown.element.style.backgroundColor="transparent";
+    }else{
+        countdown.element.style.backgroundColor="red";
+    }
+
+});
